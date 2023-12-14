c3abb230c94260209cdc7ff463e99b8bff9b85ae
anitatiwari <at309407@gmail.com>
2023-12-14 19:54:33 +0100

    cors option

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsOptions = {
   origin: 'http://localhost:3000',  //Your Client, do not write '*'
   credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
