52edcf177ce031818809c1840790e6e6f01168a0
anitatiwari <at309407@gmail.com>
2023-12-17 14:52:47 +0100

    Session Configuration

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
-import express from 'express';
-import userRouter from './routes/usersRoute.js';
-import authRouter from './routes/auth.js';
-import postsRouter from './routes/posts.js';
-import session from 'express-session';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import YAML from 'yamljs';
-import swaggerUi from 'swagger-ui-express';
+import express from "express";
+import userRouter from "./routes/usersRoute.js";
+import authRouter from "./routes/auth.js";
+import postsRouter from "./routes/posts.js";
+import session from "express-session";
+import bodyParser from "body-parser";
+import cors from "cors";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
+const app = express();
 
-const app = express()
-
-var whitelist = ['http://localhost:3000' ]
-const swaggerDocument = YAML.load('openapi.spec.yaml');
+const swaggerDocument = YAML.load("openapi.spec.yaml");
 
 const corsOptions = {
-  origin: ['http://localhost:3000','https://apiproject-frontend-main.vercel.app/','https://apiproject-frontend-main.vercel.app','http://localhost:3001','http://localhost:3002'],  //Your Client, do not write '*'
+  origin: [
+    "http://localhost:3000",
+    "https://apiproject-frontend-main.vercel.app/",
+    "http://localhost:3001",
+    "http://localhost:3002",
+  ], //Your Client, do not write '*'
   credentials: true,
 };
-app.use(
-  cors({
-    origin: ['http://localhost:3000','https://apiproject-frontend-main.vercel.app/','https://apiproject-frontend-main.vercel.app','http://localhost:3001','http://localhost:3002'],  //Your Client, do not write '*'
-    credentials: true,
-  }),
-);
+app.use(cors(corsOptions));
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'Keep it secret'
-  , name: 'uniqueSessionID'
-  , saveUninitialized: false
-}))
+app.use(
+  session({
+    secret: "Keep it secret",
+    name: "uniqueSessionID",
+    saveUninitialized: false,
+    resave: false, // Avoids session save on every request
+    rolling: true, // Reset the session timer on every request
+  })
+);
 
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
-app.use('/post', postsRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/post", postsRouter);
 
+const port = 3001;
 
-const port = 3001
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
